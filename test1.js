0e522cf8b57c6bd3b334e074ae5510b30438bec3
komal_gupta1234 <komal.gupta@intimetec.com>
2019-07-06 23:27:51 +0530

    add new line check git merge without conflict

--- test1.js
+++ test1.js
@@ -1,3 +1,5 @@
 console.log('fetch test in master')
 console.log('test file for fetch');
 console.log('add new line in master branch for git merge command only');
+
+console.log('add new line check git merge without conflict')
